935218f22190486c685ab3f18350befb287b743f
evdd86 <evdd86@hotmail.com>
2020-08-17 11:32:45 +0200

    Update Current.js
    Clean up data

--- src/Current.js
+++ src/Current.js
@@ -3,75 +3,74 @@ import axios from "axios";
 import "./Current.css";
 
 export default function Current() {
-  const [ready, setReady] = useState(false);
-  let [feel, setFeel] = useState("");
-  let [temperature, setTemperature] = useState("");
-  let [mintemp, setMintemp] = useState("");
-  let [maxtemp, setMaxtemp] = useState("");
-  let [description, setDescription] = useState("");
-  let [humidity, setHumidity] = useState("");
-  let [windspeed, setWindspeed] = useState("");
-  let [cloudiness, setCloudiness] = useState("");
-  let [icon, setIcon] = useState("");
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
   function showWeather(response) {
-    setReady(true);
-    setFeel(response.data.main.feels_like);
-    setTemperature(response.data.main.temp);
-    setMintemp(response.data.main.temp_min);
-    setMaxtemp(response.data.main.temp_max);
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWindspeed(response.data.wind.speed);
-    setCloudiness(response.data.clouds.all);
-    setIcon(response.data.weather[0].icon);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      feel: response.data.main.feels_like,
+      temperature: response.data.main.temp,
+      mintemp: response.data.main.temp_min,
+      maxtemp: response.data.main.temp_max,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      windspeed: response.data.wind.speed,
+      cloudiness: response.data.clouds.all,
+      iconUrl: `http://openweathermap.org/img/wn/${response.weather.icon}@2x.png`,
+    });
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Current">
         <div className="d-flex flex-row justify-content-center current-date">
           Mon, 27 JUL
         </div>
         <div className="d-flex flex-row justify-content-center current-location">
-          Brussels
+          {weatherData.city}
         </div>
         <div className="d-flex flex-row justify-content-center current-feel">
-          Feels like {Math.round(feel)}°C
+          Feels like {Math.round(weatherData.feel)}°C
         </div>
         <div className="d-flex flex-row justify-content-center">
           <div className="card card-current-weather">
             <div className="card-body card-body-current-weather">
               <div className="row">
-                <div className="col-6 description-current">{description}</div>
+                <div className="col-6 description-current">
+                  {weatherData.description}
+                </div>
                 <div className="col-6 min-max-current">
                   <i className="fas fa-long-arrow-alt-down"></i>{" "}
-                  {Math.round(mintemp)}°C{" "}
+                  {Math.round(weatherData.mintemp)}°C{" "}
                   <i className="fas fa-long-arrow-alt-up"></i>{" "}
-                  {Math.round(maxtemp)}°C
+                  {Math.round(weatherData.maxtemp)}°C
                 </div>
               </div>
               <div className="row">
                 <div className="col-6 emoji-current">
                   <img
                     className="icon"
-                    src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-                    alt="icon"
+                    src={weatherData.iconUrl}
+                    alt={weatherData.description}
                   ></img>
                 </div>
                 <div className="col-6 temperature-current">
-                  {Math.round(temperature)}°C
+                  {Math.round(weatherData.temperature)}°C
                 </div>
               </div>
               <div className="row">
                 <div className="col-4 humidity-current">
-                  <i className="fas fa-tint"></i> {Math.round(humidity)}%
+                  <i className="fas fa-tint"></i>{" "}
+                  {Math.round(weatherData.humidity)}%
                 </div>
                 <div className="col-4 windspeed-current">
-                  <i className="fas fa-wind"></i> {Math.round(windspeed)} KM/H
+                  <i className="fas fa-wind"></i>{" "}
+                  {Math.round(weatherData.windspeed)} KM/H
                 </div>
                 <div className="col-4 cloudiness-current">
-                  <i className="fas fa-cloud"></i> {Math.round(cloudiness)}%
+                  <i className="fas fa-cloud"></i>{" "}
+                  {Math.round(weatherData.cloudiness)}%
                 </div>
               </div>
             </div>
